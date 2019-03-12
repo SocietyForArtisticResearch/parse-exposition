f381d2736be9ce4274d7f1219ab31b78f8e3fef1
Luc Döbereiner <luc.doebereiner@gmail.com>
2019-03-12 18:10:39 +0100

    download, video and pandoc

--- README.md
+++ README.md
@@ -14,10 +14,16 @@ It only works for published expositions at the moment
 ## Build and run
 ```stack build --exec "parse-exposition https://www.researchcatalogue.net/view/343349/343350"```
 
+## Options
+* `-m` for markdown conversion
+* `-d` for download of media files (into "./media" folder)
+
+```stack build --exec "parse-exposition -d -m https://www.researchcatalogue.net/view/384876/384878"```
+
+
 ## Run only
 ```stack exec parse-exposition https://www.researchcatalogue.net/view/343349/343350```
 
 ## TODO
-* Only text tools are being read at the moment.
-* Optional conversion to markdow using pandoc lib
-* Make metadata from jsonld meta block optional in order to enable non-published expos
+* Download all weaves of an exposition
+* Make metadata from jsonld meta block optional in order to enable non-published expos (perhaps get metadata from menubar)
--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,11 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedStrings #-}
 
+import Control.Monad
 import Data.Aeson (FromJSON, ToJSON)
 import Data.Aeson.Encode.Pretty (encodePretty)
+import Data.Conduit ((.|), runConduitRes)
+import qualified Data.Conduit.Binary as CB
 import qualified Data.List as L
 import qualified Data.Map.Strict as M
 import qualified Data.Text as T
@@ -12,9 +15,11 @@ import Data.Text.Lazy (toStrict)
 import Data.Text.Lazy.Encoding (decodeUtf8)
 import Data.Text.Read (decimal)
 import GHC.Generics
-import Network.HTTP.Simple (httpSink, parseRequest)
+import Network.HTTP.Simple (getResponseBody, httpSink, httpSource, parseRequest)
+import System.Directory (createDirectory, doesDirectoryExist)
 import System.Environment
 import System.Exit
+import System.FilePath ((</>), takeExtension)
 import Text.Blaze.Html (toHtml)
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import Text.HTML.DOM (sinkDoc)
@@ -63,18 +68,20 @@ data Size = Size
 instance ToJSON (Size)
 
 data Tool = Tool
-  { id :: T.Text
+  { toolMediaFile :: Maybe String
+  , toolId :: T.Text
   , position :: Position
   , size :: Size
-  , content :: ToolContent
+  , toolContent :: ToolContent
   } deriving (Generic, Show)
 
 instance ToJSON (Tool)
 
 -- | Content types to be extended
 data ToolContent
-  = TextContent { content :: T.Text }
+  = TextContent { textToolContent :: T.Text }
   | ImageContent { imageUrl :: T.Text }
+  | VideoContent { videoUrl :: T.Text }
   deriving (Generic, Show)
 
 instance ToJSON (ToolContent)
@@ -82,6 +89,7 @@ instance ToJSON (ToolContent)
 data ImportOptions = ImportOptions
   { markdown :: Bool
   , url :: String
+  , download :: Maybe String
   }
 
 type ToolTypeName = T.Text
@@ -133,6 +141,9 @@ extractSizePos styles = (positions, sizes)
     heights = map (styleProperty "height") assocStyles
     sizes = Size <$> widths <*> heights
 
+--------------------
+--- Specification of tool functions
+--------------------
 textContent :: Cursor Node -> ToolContent
 textContent cursor =
   TextContent $ (toStrict . renderHtml . toHtml . node) cursor
@@ -142,16 +153,19 @@ imageContent cursor = ImageContent img
   where
     img = T.concat $ cursor $// element "img" >=> attribute "src"
 
-textSpec :: ToolSpec
-textSpec = ("text", textContent)
+videoContent :: Cursor Node -> ToolContent
+videoContent cursor = VideoContent video
+  where
+    video = T.concat $ cursor $// attribute "data-file"
 
-imageSpec :: ToolSpec
-imageSpec = ("picture", imageContent)
+toolSpecs :: [ToolSpec]
+toolSpecs =
+  [("text", textContent), ("picture", imageContent), ("video", videoContent)]
 
 -- | Get tools of a certain type
 getTools :: ToolSpec -> Cursor Node -> [Tool]
 getTools (toolTypeName, contentFun) cursor =
-  L.zipWith4 Tool ids positions sizes toolContent
+  L.zipWith4 (Tool Nothing) ids positions sizes toolContent
   where
     tools = cursor $// attributeIs "data-tool" toolTypeName
     ids = map (T.concat . attribute "data-id") tools
@@ -162,36 +176,132 @@ getTools (toolTypeName, contentFun) cursor =
       check (checkClass "tool-content")
     toolContent = map contentFun content
 
+--------------------
+--- File download
+--------------------
+downloadFile :: String -> String -> IO ()
+downloadFile url fname = do
+  req <- (parseRequest url)
+  runConduitRes $ httpSource req getResponseBody .| CB.sinkFile fname
+
+toolUrl :: Tool -> Maybe String
+toolUrl tool =
+  case toolContent tool of
+    ImageContent url -> Just (T.unpack url)
+    VideoContent url -> Just (T.unpack url)
+    _ -> Nothing
+
+toolFileExtension :: Tool -> Maybe String
+toolFileExtension tool =
+  let fname url = Just $ (takeWhile (/= '?') $ takeExtension url)
+   in fname =<< toolUrl tool
+
+toolFileName :: Tool -> String -> Maybe String
+toolFileName tool dir =
+  (\ending -> dir </> (T.unpack $ toolId tool) ++ ending) <$>
+  toolFileExtension tool
+
+downloadTool :: String -> Tool -> IO ()
+downloadTool dir tool =
+  let fileName = toolFileName tool dir
+      url = toolUrl tool
+   in case (fileName, url) of
+        (Just fnameStr, Just urlStr) -> do
+          downloadFile urlStr fnameStr
+        _ -> do
+          return ()
+
+downloadTools :: ImportOptions -> [Tool] -> IO ()
+downloadTools options tools =
+  case download options of
+    Nothing -> do
+      return ()
+    Just dir -> do
+      exists <- doesDirectoryExist dir
+      if not exists
+        then createDirectory dir
+        else return ()
+      mapM (downloadTool dir) tools
+      return ()
+
+--------------------
+--- Markdown conversion
+--------------------
+toolToMarkdown :: Pan.PandocMonad m => Tool -> m Tool
+toolToMarkdown tool =
+  case toolContent tool of
+    TextContent txt -> do
+      pantxt <- Pan.readHtml PanOptions.def txt
+      mdtxt <- Pan.writeMarkdown PanOptions.def pantxt
+      return tool {toolContent = TextContent mdtxt}
+    _ -> do
+      return tool
+
+expToMarkdown :: Pan.PandocMonad m => Exposition -> m Exposition
+expToMarkdown exp = do
+  mdTools <- mapM toolToMarkdown (tools exp)
+  return $ exp {tools = mdTools}
+
+-- writeExpMarkdown :: Exposition -> IO ()
+-- writeExpMarkdown exp = do
+--   md <- liftM expToMarkdown exp
+--   TIO.putStrLn $ encodeTxt md
+--------------------
+--- Main functions
+--------------------
+insertFileNames :: ImportOptions -> Exposition -> Exposition
+insertFileNames options exp =
+  case download options of
+    Nothing -> exp
+    Just dir ->
+      exp
+        { tools =
+            map
+              (\tool -> tool {toolMediaFile = toolFileName tool dir})
+              (tools exp)
+        }
+
 parseExposition :: Cursor Node -> Exposition
 parseExposition cursor =
   Exposition
     { title = getMeta cursor "citation_title"
     , author = getMeta cursor "citation_author"
     , date = getMeta cursor "citation_publication_date"
-    , tools = getTools textSpec cursor ++ getTools imageSpec cursor
+    , tools = L.concat $ map (\spec -> getTools spec cursor) toolSpecs
     }
 
 getExposition :: ImportOptions -> IO Exposition
 getExposition options = do
   req <- parseRequest (url options)
   doc <- httpSink req $ const sinkDoc
-  return $ parseExposition (fromDocument doc)
+  return $ insertFileNames options $ parseExposition (fromDocument doc)
 
 parseArgs :: [String] -> ImportOptions
-parseArgs args = makeOptions args (ImportOptions False "")
+parseArgs args = makeOptions args (ImportOptions False "" Nothing)
   where
     makeOptions :: [String] -> ImportOptions -> ImportOptions
     makeOptions ("-m":t) options = makeOptions t (options {markdown = True})
+    makeOptions ("-d":t) options =
+      makeOptions t (options {download = Just "media"})
     makeOptions (url:t) options = makeOptions t (options {url = url})
     makeOptions [] options = options
 
-usage = putStrLn "Usage: parse-exposition [-m] rc-url"
+usage = putStrLn "Usage: parse-exposition [-m] [-d] rc-url"
 
 exit = exitWith ExitSuccess
+
+expToTxt :: Pan.PandocMonad m => m Exposition -> m T.Text
+expToTxt exp = encodeTxt <$> exp
+
+encodeMdTxt :: Exposition -> IO T.Text
+encodeMdTxt exp = Pan.runIOorExplode $ expToTxt (expToMarkdown exp)
 
 main :: IO ()
 main = do
   args <- getArgs
-  exp <- getExposition $ parseArgs args
-  -- TODO deal with markdown conversion here
-  TIO.putStrLn $ encodeTxt exp
+  let options = parseArgs args
+  exp <- getExposition options
+  downloadTools options (tools exp)
+  if markdown options
+    then TIO.putStrLn =<< encodeMdTxt exp
+    else TIO.putStrLn $ encodeTxt exp
